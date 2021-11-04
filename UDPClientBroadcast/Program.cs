6ff31181ea20d6ca71b747fbe623c8a5ba5283d1
move <move@zealand.dk>
2021-11-04 11:12:52 +0100

    Add project files.

---
+++ UDPClientBroadcast/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPClientBroadcast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            UdpClient socket = new UdpClient();
+            SendMessage(socket, message);
+        }
+
+        public static void SendMessage(UdpClient socket, string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            socket.Send(data, data.Length, "255.255.255.255", 65000);
+
+            while (true)
+            {
+                IPEndPoint from = null;
+                byte[] received = socket.Receive(ref from);
+                string receivedString = Encoding.UTF8.GetString(received);
+                Console.WriteLine(receivedString + " - " + from.Address);
+            }
+        }
+    }
+}
---
+++ UDPEcho/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPEcho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UDP Echo Server");
+
+            //initialize
+            UdpClient socket = new UdpClient();
+            socket.Client.Bind(new IPEndPoint(IPAddress.Any, 65000));
+
+            while (true)
+            {
+                //receieve data
+                IPEndPoint from = null;
+                byte[] data = socket.Receive(ref from);
+                string dataString = Encoding.UTF8.GetString(data);
+                Console.WriteLine("Received from client: " + dataString + " - " + from.Address);
+
+                dataString = "move: " + dataString;
+                byte[] toBeSend = Encoding.UTF8.GetBytes(dataString);
+                //echo back data
+                socket.Send(toBeSend, toBeSend.Length, from);
+            }
+        }
+    }
+}
